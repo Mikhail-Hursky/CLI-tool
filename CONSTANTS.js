507d6b59a79126b1a5a8e7f36bfc5a7e5e117695
m-hurski <85618717+m-hurski@users.noreply.github.com>
2021-10-31 16:25:30 +0300

    add encode func

---
+++ CONSTANTS.js
@@ -0,0 +1,4 @@
+const UPPERCASE_ARRAY = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] // 26
+const LOWERCASE_ARRAY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // 26
+
+module.exports = {UPPERCASE_ARRAY, LOWERCASE_ARRAY}
---
+++ encode.js
@@ -0,0 +1,19 @@
+const {UPPERCASE_ARRAY, LOWERCASE_ARRAY} = require('./CONSTANTS')
+
+const encode = (str, k) => {
+  if (k % 26 === 0) return str;
+  while (k < 0) k += 26
+  while (k > 26) k -= 26
+  return str.split('').map((el) => {
+    if (UPPERCASE_ARRAY.indexOf(el) !== -1) return encodeSup(el, UPPERCASE_ARRAY, k)
+    if (LOWERCASE_ARRAY.indexOf(el) !== -1) return encodeSup(el, LOWERCASE_ARRAY, k)
+    return el
+  }).join('')
+}
+
+function encodeSup (el, arr, k) {
+  if (arr[arr.indexOf(el) + k]) return arr[arr.indexOf(el) + k]
+  else return arr[arr.indexOf(el) + k - 26]
+}
+
+module.exports = encode
