d1978b8020dfa78142f3e0ad1db4e7539815e2ea
m-hurski <85618717+m-hurski@users.noreply.github.com>
2021-11-09 22:33:01 +0300

    finish task

--- Utils/atbashCipher.js
+++ Utils/atbashCipher.js
@@ -1,9 +1,9 @@
-const {UPPERCASE_ARRAY, LOWERCASE_ARRAY} = require('./CONSTANTS')
+const {UPPERCASE_ARRAY, LOWERCASE_ARRAY} = require('../Constants/CONSTANTS')
 
 const atbashCipher = (str) => {
   const UpArr = [...UPPERCASE_ARRAY].reverse()
   const LoArr = [...LOWERCASE_ARRAY].reverse()
-  return str.split('').reduce((acc, el) => {
+  return [...str].reduce((acc, el) => {
     if (UPPERCASE_ARRAY.indexOf(el) !== -1) return `${acc}${UpArr[UPPERCASE_ARRAY.indexOf(el)]}`
     if (LOWERCASE_ARRAY.indexOf(el) !== -1) return `${acc}${LoArr[LOWERCASE_ARRAY.indexOf(el)]}`
     return `${acc}${el}`
--- Utils/caesarCipher.js
+++ Utils/caesarCipher.js
@@ -1,14 +1,14 @@
-const {UPPERCASE_ARRAY, LOWERCASE_ARRAY} = require('./CONSTANTS')
+const {UPPERCASE_ARRAY, LOWERCASE_ARRAY} = require('../Constants/CONSTANTS')
 
 const caesarCipher = (str, k) => {
   if (k % 26 === 0) return str;
   while (k < 0) k += 26
   while (k > 26) k -= 26
-  return str.split('').map((el) => {
+  return [...str].map((el) => {
     if (UPPERCASE_ARRAY.indexOf(el) !== -1) return encodeSup(el, UPPERCASE_ARRAY, k)
     if (LOWERCASE_ARRAY.indexOf(el) !== -1) return encodeSup(el, LOWERCASE_ARRAY, k)
     return el
-  }).join('')
+  }).join(``)
 }
 
 function encodeSup (el, arr, k) {
