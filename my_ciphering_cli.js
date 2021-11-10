0f64c65773011d4f44e04fdbf8bb7f6a0bcd4296
m-hurski <85618717+m-hurski@users.noreply.github.com>
2021-11-10 09:14:04 +0300

    delete fo PR

--- my_ciphering_cli.js
+++
@@ -1,5 +0,0 @@
-const {switcher, arrMethods, inputStream, outPutStream} = require("./createStreams");
-
-inputStream.on('data', function (chunk) {
-  outPutStream.write(switcher(chunk, arrMethods));
-});
